d72a83b8f63d0ad27e17cafc11378eb442a213cd
leverh <leverh@gmail.com>
2023-02-12 12:51:27 +0100

    Fix error on reset button

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     let inputs = document.getElementsByTagName("input");
     let resetButton = document.getElementById("reset-btn");
+  
+    for (let input of inputs) {
+      input.addEventListener("click", function () {
+        console.log("Data-type: " + this.getAttribute("data-type"));
+        if (this.getAttribute("data-type") !== "reset") {
+          let userInput = this.getAttribute("data-type");
+          let computerInput = computersTurn();
+          compareChoices(userInput, choices[computerInput]);
+        }
+      });
+    }
+    resetButton.addEventListener("click", function () {
+      console.log("Data-type: " + this.getAttribute("data-type"));
+      if (this.getAttribute("data-type") === "reset") {
+        resetGame();
+      }
+    });
+  });
